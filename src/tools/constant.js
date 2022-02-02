53dd3f8303cfc260172ff3c0ad1cdbd402e36989
andi <andy.u789@gmail.com>
2022-02-02 09:33:55 +0200

    Updates

--- src/tools/constant.js
+++
@@ -1,6 +0,0 @@
-// imageCreate.js, imageToText.js
-export const FONT_WIDTH = 6
-export const FONT_HEIGHT = 12
-
-// imageToText.js
-export const DEFAULT_AVAILABLE_TEXTS = '@#&$%863!i1uazvno~;*^+-. '
